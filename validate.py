674fca848ccb81354c8cbc4d181279dac47a8978
Marcello de Sales <Marcello_DeSales@intuit.com>
2016-09-27 18:48:43 -0700

    CFG-174: Validate Json files and output message

--- validate.py
+++ validate.py
@@ -16,21 +16,42 @@ def listAllConfigFiles():
 
   return allConfigs
 
+# http://stackoverflow.com/questions/11294535/verify-if-a-string-is-json-in-python/11294685#11294685
+def isJsonFileValid(filePath):
+  print "Validating " + filePath
+  try:
+    with open(filePath) as data_file:
+      data = json.load(data_file)
+    return True
+
+  except ValueError, e:
+    return False
+
 def validateConfigs():
+  # The index of the files and if they are valid
+  fileValidatesIndex = {}
+
+  # Iterate over all config files, validating according to their extension
   for configFileName in listAllConfigFiles():
     if configFileName.endswith(".json"):
-      print "File " + configFileName + " is json"
+      fileValidatesIndex[configFileName] = isJsonFileValid(configFileName)
 
     elif configFileName.endswith(".yml") or configFileName.endswith(".yaml"):
-      print "File " + configFileName + " is yaml"
+      fileValidatesIndex[configFileName] = True
 
     else:
-      print "File " + configFileName + " is properties"
+      fileValidatesIndex[configFileName] = True
+
+  return fileValidatesIndex
 
 print "All configs"
 print listAllConfigFiles()
 
-validateConfigs()
-
-
-
+validationIndex = validateConfigs()
+for filePath, isValid in validationIndex.iteritems():
+  if isValid:
+    # http://www.fileformat.info/info/unicode/char/2714/index.htm
+    print str(u'\u2714'.encode('UTF-8')) + " File " + filePath + " is valid!"
+  else:
+    # http://www.fileformat.info/info/unicode/char/2718/index.htm
+    print str(u'\u2718'.encode('UTF-8')) + " File " + filePath + " is NOT valid!"
