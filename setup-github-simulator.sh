479b1ebbde0a18acf8ea0a81b65cef800b745753
Marcello de Sales <Marcello_deSales@intuit.com>
2019-04-26 11:14:02 -0700

    Simulator: Fix setup and gitignore

--- setup-github-simulator.sh
+++ setup-github-simulator.sh
@@ -27,33 +27,22 @@ echo ""
 
 DOCKER_IMAGE=intuit/intuit-spring-cloud-config-validator:${VERSION}
 
-echo "${red}==========--------- Start the data container -----------=========="
-echo ""
-echo "${green}* docker run --name data ${DOCKER_IMAGE} /bin/true${yellow}"
-echo ""
-docker stop data || true
-docker rm data || true
-docker run --name data ${DOCKER_IMAGE} /bin/true
-#echo "Copy the current validator script to the data container If needed for testing without rebuilding
-#docker cp validate_config_files.py data:/home/git/test.git/hooks/pre-receive
-echo ""
-
 echo "${red}==========--------- Start a new Github Server simulator -----------=========="
 echo ""
-echo "${green}* docker run --name gitrepo -e GITHUB_PULL_REQUEST_HEAD=00000000 -e GITHUB_PULL_REQUEST_BASE=f394274 -d -p 52311:22 --volumes-from data -e GIT_DIR=/home/git/test.git ${DOCKER_IMAGE}${yellow}"
+echo "${green}* docker run --name gitrepo -e GITHUB_PULL_REQUEST_HEAD=00000000 -e GITHUB_PULL_REQUEST_BASE=f394274 -d -p 52311:22 -e GIT_DIR=/home/git/test.git ${DOCKER_IMAGE}${yellow}"
 echo ""
 docker stop gitrepo || true
 docker rm gitrepo || true
-docker run --name gitrepo -e GITHUB_PULL_REQUEST_HEAD=00000000 -e GITHUB_PULL_REQUEST_BASE=f394274 -d -p 52311:22 --volumes-from data -e GIT_DIR=/home/git/test.git ${DOCKER_IMAGE}
+docker run --name gitrepo -e GITHUB_PULL_REQUEST_HEAD=00000000 -e GITHUB_PULL_REQUEST_BASE=f394274 -d -p 52311:22 -e GIT_DIR=/home/git/test.git ${DOCKER_IMAGE}
 echo ""
 
 SIMULATOR_SSH_PEM=$(pwd)/.id_rsa_from_github_simulator_server
 
 echo "${red}==========--------- Copying credentials from Github server Container -----------=========="
 echo ""
-echo "${green}* docker cp data:/home/git/.ssh/id_rsa ${SIMULATOR_SSH_PEM}${yellow}"
+echo "${green}* docker cp gitrepo:/home/git/.ssh/id_rsa ${SIMULATOR_SSH_PEM}${yellow}"
 echo ""
-docker cp data:/home/git/.ssh/id_rsa ${SIMULATOR_SSH_PEM}
+docker cp gitrepo:/home/git/.ssh/id_rsa ${SIMULATOR_SSH_PEM}
 echo ""
 
 date
